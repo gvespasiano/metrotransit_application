751a20f7592d92499159ff46a21bd721d7e67d58
gvespasiano <gvespasiano@gmail.com>
2019-05-11 19:38:54 -0500

    Full list of gets from API, HTML structure, auto refresh at 4.5mins

--- js/script.js
+++ js/script.js
@@ -2,17 +2,44 @@ var baseURL = "https://svc.metrotransit.org";
 var vm = new Vue({
     el: "#app",
     data: {
-        train: [],
+        eP1: [],
+		eP2: [],
+		wP1: [],
+		wP2: [],
     },
     methods: {
-        getTrain: function() {
+        getEP1: function() {
             axios.get(baseURL + "/NexTrip/56335").then(function(response){
-                vm.train = response.data;
+                vm.eP1 = response.data;
             })
-        }
+        },
+        getEP2: function() {
+            axios.get(baseURL + "/NexTrip/56334").then(function(response){
+                vm.eP2 = response.data;
+            })
+        },
+        getWP1: function() {
+            axios.get(baseURL + "/NexTrip/56333").then(function(response){
+                vm.wP1 = response.data;
+            })
+        },
+        getWP2: function() {
+            axios.get(baseURL + "/NexTrip/56339").then(function(response){
+                vm.wP2 = response.data;
+            })
+        },
     },
     created: function() {
-        this.getTrain();
+        this.getEP1();
+		this.getEP2();
+		this.getWP1();
+		this.getWP2();
     }
 });
+
+//auto refresh every 4.5 mins
+setTimeout(function() {
+  location.reload();
+}, 270000);
+
 
